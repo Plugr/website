c051a4df74ed95c90cae58dee12292668cc25753
Rohan Iyer <rohan.iyer1999@gmail.com>
2015-07-02 14:14:19 -0700

    changes

--- datagenerator.js
+++ datagenerator.js
@@ -1,24 +1,25 @@
 var Firebase = require('firebase');
 
-var ref = new Firebase('https://plugr.firebaseio.com/');
+var ref = new Firebase('https://hiveio.firebaseio.com/');
 
+var user = 'riyer' //changes based on user input    
 
 setInterval(function(){
     
-    if(ref.child('users').child('riyer').child('port1').child('type') != ''){
+    if(ref.child('users').child('riyer').child('port1').child('type') != 'empty'){
         ref.child('users').child('riyer').child('port1').update({
             value: Math.random() * 1000   
         })
     }
                                                  
-    if(ref.child('users').child('riyer').child('port2').child('type') != ''){
+    if(ref.child('users').child('riyer').child('port2').child('type') != 'empty'){
         ref.child('users').child('riyer').child('port2').update({
             value: Math.random() * 1000   
         })
     }  
                                                  
     
-    if(ref.child('users').child('riyer').child('port3').child('type') != ''){
+    if(ref.child('users').child('riyer').child('port3').child('type') != 'empty'){
         ref.child('users').child('riyer').child('port3').update({
             value: Math.random() * 1000   
         })
